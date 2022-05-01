ed99df7918bf6734e1bf998d383b04313fb21735
faysalahmed000012 <faysal000012@gmail.com>
2022-05-01 16:50:37 +0600

    inventory loaded

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Header from "./Components/Shared/Header/Header";
 import Footer from "./Components/Shared/Footer/Footer";
 import { Route, Routes } from "react-router-dom";
 import Blogs from "./Components/Blogs/Blogs";
+import Inventories from "./Components/Home/Inventories/Inventories";
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Inventories></Inventories>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/home" element={<Home></Home>}></Route>
--- src/Components/Home/Inventories/Inventories.js
+++ src/Components/Home/Inventories/Inventories.js
@@ -1,9 +1,22 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import Inventory from "../Inventory/Inventory";
 
 const Inventories = () => {
+  const [monitors, setMonitors] = useState([]);
+  useEffect(() => {
+    const url = `http://localhost:5000/inventories`;
+    axios.get(url).then((response) => setMonitors(response.data));
+  }, []);
   return (
     <div>
-      <h2>Load inventories here</h2>
+      <h2>Load inventories here : {monitors.length}</h2>
+      <div className="monitors-container container mx-auto grid grid-cols-3">
+        {monitors.map((monitor) => (
+          <Inventory key={monitor._id} monitor={monitor}></Inventory>
+        ))}
+      </div>
     </div>
   );
 };
